9e564399874c31fc0be1ea6583be2e5bf268011d
mil <1363944642@qq.com>
2020-11-08 20:51:33 +0800

    City-侧边栏与城市列表组件的联动_侧边栏滚动的节流优化_修复滚动bug_

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,7 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
           <div
@@ -39,53 +39,81 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
-  mounted() {
-    setTimeout(() => {
-      this.scroll = new Bscroll(this.$refs.wrapper, {})
-    }, 500)
+  updated() {
+    this.scroll = new Bscroll(this.$refs.wrapper, {})
+  },
+  watch: {
+    letter() {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.border-topbottom
-  &:before
-    border-color: #ccc
-  &:after
-    border-color: #ccc
-.border-bottom
-  &:before
-    border-color: #ccc
-.list
-  overflow: hidden
-  position: absolute
-  top: 1.58rem
-  left: 0
-  right: 0
-  bottom: 0
-  .title
-    line-height: .54rem
-    background: #eee
-    padding-left: .2rem
-    color: #666
-    font-size: .26rem
-  .button-list
-    overflow: hidden
-    padding: .1rem .6rem .1rem .1rem
-    .button-wrapper
-      float: left
-      width: 33.33%
-      .button
-        margin: .1rem
-        padding: .1rem 0
-        text-align: center
-        border: .02rem solid #ccc
-        border-radius: 0.06rem
-  .item-list
-    .item
-      line-height: .76rem
-      padding-left: .2rem
+@import '~styles/varibles.styl';
+
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
+  }
+
+  &:after {
+    border-color: #ccc;
+  }
+}
+
+.border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+}
+
+.list {
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  .title {
+    line-height: 0.54rem;
+    background: #eee;
+    padding-left: 0.2rem;
+    color: #666;
+    font-size: 0.26rem;
+  }
+
+  .button-list {
+    overflow: hidden;
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+
+    .button-wrapper {
+      float: left;
+      width: 33.33%;
+
+      .button {
+        margin: 0.1rem;
+        padding: 0.1rem 0;
+        text-align: center;
+        border: 0.02rem solid #ccc;
+        border-radius: 0.06rem;
+      }
+    }
+  }
+
+  .item-list {
+    .item {
+      line-height: 0.76rem;
+      padding-left: 0.2rem;
+    }
+  }
+}
 </style>
